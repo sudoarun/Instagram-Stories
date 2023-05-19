7596d6ae66b49b053f5737aa53bab9fa227ced8a
sudoarun <arunrajorya1999@gmail.com>
2023-05-19 16:21:16 +0530

    added images and gradient

--- components/InstaStories.js
+++ components/InstaStories.js
@@ -1,5 +1,6 @@
 import * as react from "react";
 // import { Text, View } from "react-native-paper";
+import { LinearGradient } from "expo-linear-gradient";
 import { Image, ScrollView, StyleSheet, Text, View } from "react-native";
 import user from "../user.json";
 
@@ -7,14 +8,20 @@ const InstaStories = () => {
   return (
     <ScrollView horizontal showsHorizontalScrollIndicator={false}>
       {user.map((data, index) => (
-        <View key={index} style={{ width: 100 }}>
-          <Image
-            source={{
-              uri: data.img,
-            }}
-            style={style.img}
-          />
-          {/* <Text>{data.name}</Text> */}
+        <View key={index} style={{ width: 100, flex: 1, alignItems: "center" }}>
+          <LinearGradient
+            colors={["#4c669f", "#3b5998", "#192f6a"]}
+            style={{ padding: 3, borderRadius: 50 }}
+          >
+            <Image
+              source={{
+                uri: data.img,
+              }}
+              style={style.img}
+            />
+          </LinearGradient>
+
+          <Text>{data.name}</Text>
         </View>
       ))}
     </ScrollView>
