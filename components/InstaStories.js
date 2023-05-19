4380ef7406d3e3897042680dba02d13ea25fa9d8
sudoarun <arunrajorya1999@gmail.com>
2023-05-19 16:13:08 +0530

    added images

--- components/InstaStories.js
+++ components/InstaStories.js
@@ -1,17 +1,30 @@
 import * as react from "react";
 // import { Text, View } from "react-native-paper";
-import { ScrollView, Text, View } from "react-native";
+import { Image, ScrollView, StyleSheet, Text, View } from "react-native";
 import user from "../user.json";
 
 const InstaStories = () => {
   return (
-    <ScrollView horizontal>
+    <ScrollView horizontal showsHorizontalScrollIndicator={false}>
       {user.map((data, index) => (
-        <View key={index}>
-          <Text>{data.name}</Text>
+        <View key={index} style={{ width: 100 }}>
+          <Image
+            source={{
+              uri: data.img,
+            }}
+            style={style.img}
+          />
+          {/* <Text>{data.name}</Text> */}
         </View>
       ))}
     </ScrollView>
   );
 };
+const style = StyleSheet.create({
+  img: {
+    height: 70,
+    width: 70,
+    borderRadius: 50,
+  },
+});
 export default InstaStories;
